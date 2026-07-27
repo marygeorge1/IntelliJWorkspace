ef8309fd638e1a9bcbb2dedacd5e4ce472bca705
Mary <marygeorge.ed@gmail.com>
2026-07-27 14:51:46 +0100

    Added eureka server

--- AI Powered Fitness Application/activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ AI Powered Fitness Application/activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ActivityserviceApplication {
 
 	public static void main(String[] args) {
--- AI Powered Fitness Application/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ AI Powered Fitness Application/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,7 +5,7 @@ import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
